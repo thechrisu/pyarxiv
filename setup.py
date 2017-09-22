88af15232accedd75c8c9f0f1e6b19defbb75229
culshoefer <christophsulshoefer@gmail.com>
2017-09-22 10:01:16 -0400

    ADDED: Project skeleton

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='pyarxiv',
+      version='1.0',
+      install_requires=requirements,
+      description='Python Client Library and CLI client for the ArXiv.org API',
+      author='Christoph Ulshoefer',
+      author_email='c@culshoefer.com',
+      packages=find_packages(),
+      url='https://github.com/culshoefer/pyarxiv/',
+      test_suite='tests',
