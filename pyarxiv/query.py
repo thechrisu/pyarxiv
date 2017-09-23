81e131edbca8be5df3673841cd2d5ec321a7702b
culshoefer <christophsulshoefer@gmail.com>
2017-09-23 12:06:21 -0400

    ADDED: Query module

---
+++ pyarxiv/query.py
@@ -0,0 +1,72 @@
+from urllib import parse
+from urllib import request
+import feedparser
+
+ARXIV_API_BASE_URI = 'http://export.arxiv.org/api/query?'
+
+
+def query(max_results=100, ids=[], categories=[],
+          title='', authors='', abstract='', journal_ref='',
+          querystring=''):
+    """
+    :param max_results: Max number of results, by default 100.
+    :param ids: arXiv ids of entries to be found (ORed together).
+    :param categories: A valid entry is e.g. ['math.AG', 'cs.AI']
+    to search for papers in Algebraic Geometry and AI.
+    :param title: Restrict search to papers with this string in their title.
+    :param authors: Restrict search with this string in author name(s).
+    :param abstract: Restrict search with this string in abstract.
+    :param journal_ref: Restrict search to e.g. 'Phys Rev Lett'.
+    :param querystring: Simply enter a query string ('manual mode').
+    This query string must be properly escaped as by the arXiv API docs:
+    https://arxiv.org/help/api/user-manual#query_details
+    If this argument is present, all other values,
+    except for max_results and ids are ignored.
+    :return: list of dictionaries of arXiv entries matching query.
+    """
+    if len(querystring) > 0:
+        real_querystring = querystring
+    else:
+        real_querystring = get_querystring(categories,
+                                           title,
+                                           authors,
+                                           abstract,
+                                           journal_ref)
+    search_query = "&search_query=" + real_querystring
+    query = 'max_results=%i' % max_results
+    if len(real_querystring) > 0:
+        query += search_query
+    if len(ids) > 0:
+        query += "&id_list=" + ",".join(ids)
+    raw_d = request.urlopen(
+        ARXIV_API_BASE_URI + query).read()
+    d = feedparser.parse(raw_d)
+    return d.entries
+
+
+def get_querystring(categories=[], title='', authors='',
+                    abstract='', journal_ref=''):
+    """
+    Helper function for query() builds up a custom search query.
+    :param categories: categories to be used.
+    :param title: title of papers.
+    :param authors: authors.
+    :param abstract: abstract.
+    :param journal_ref: journal ref.
+    :return: Properly escaped search query.
+    """
+    query_elements = []
+    if len(categories) > 0 and isinstance(categories, list):
+        used_categories = " OR ".join(
+            list(map(lambda x: 'cat:' + x, categories)))
+        query_elements.append("(" + used_categories + ")")
+    if len(title) > 0:
+        query_elements.append("ti:\"" + title + "\"")
+    if len(authors) > 0:
+        query_elements.append("au:\"" + authors + "\"")
+    if len(abstract) > 0:
+        query_elements.append("abs:\"" + abstract + "\"")
+    if len(journal_ref) > 0:
+        query_elements.append("jr:\"" + journal_ref + "\"")
+    built_query = " AND ".join(query_elements)
+    return parse.quote_plus(built_query, safe=':+')
---
+++ tests/test_query.py
@@ -0,0 +1,97 @@
+import unittest
+from unittest.mock import patch, Mock
+
+import pyarxiv.query as paq
+
+
+class TestQuery(unittest.TestCase):
+    @patch('feedparser.parse')
+    @patch('urllib.request.urlopen')
+    def test_default(self,
+                     mock_req,
+                     mock_parse):
+        parse_ret = Mock()
+        parse_ret.entries = 'asdf'
+        mock_parse.return_value = parse_ret
+        self.assertEqual(paq.query(max_results=100), 'asdf')
+        mock_req.assert_called_with(
+            "http://export.arxiv.org/api/query?max_results=100")
+        mock_parse.assert_called_once()
+
+    @patch('feedparser.parse')
+    @patch('urllib.request.urlopen')
+    def test_querystring_provided_overrides_others_except_for_id(self,
+                                                                 mock_req,
+                                                                 mock_parse):
+        paq.query(max_results=100,
+                  ids=['1'],
+                  authors='asdf',
+                  querystring='somequerystring')
+        mock_req.assert_called_with(
+            "http://export.arxiv.org/api/query?max_results=100"
+            "&search_query=somequerystring&id_list=1")
+
+    @patch('feedparser.parse')
+    @patch('urllib.request.urlopen')
+    def test_id_string_comma_separated(self,
+                                       mock_req,
+                                       mock_parse):
+        paq.query(ids=['1', '2'])
+        mock_req.assert_called_with(
+            "http://export.arxiv.org/api/query?max_results=100"
+            "&id_list=1,2")
+
+    @patch('feedparser.parse')
+    @patch('urllib.request.urlopen')
+    def test_max_results_always_there(self,
+                                      mock_req,
+                                      mock_parse):
+        paq.query()
+        mock_req.assert_called_with(
+            "http://export.arxiv.org/api/query?max_results=100")
+
+
+class TestQueryConstruction(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(paq.get_querystring(), '')
+
+    def test_categories(self):
+        self.assertEqual(paq.get_querystring('somestring'),
+                         '')
+        self.assertEqual(paq.get_querystring(['randomString']),
+                         '%28cat:randomString%29')
+        self.assertEqual(paq.get_querystring(['randcat', 'rand2']),
+                         '%28cat:randcat+OR+cat:rand2%29')
+
+    def test_title(self):
+        self.assertEqual(paq.get_querystring(title=''), '')
+        self.assertEqual(paq.get_querystring(title='some random title'),
+                         'ti:%22some+random+title%22')
+
+    def test_authors(self):
+        self.assertEqual(paq.get_querystring(authors='some author name'),
+                         'au:%22some+author+name%22')
+
+    def test_abstract(self):
+        self.assertEqual(paq.get_querystring(abstract='some abstract'),
+                         'abs:%22some+abstract%22')
+
+    def test_journal_ref(self):
+        self.assertEqual(paq.get_querystring(journal_ref='Phys Rev Lett'),
+                         'jr:%22Phys+Rev+Lett%22')
+
+    def test_all_together_now(self):
+        self.assertEqual(paq.get_querystring(['cs.AI', 'cs.INVENTED'],
+                                             'some title',
+                                             'some author',
+                                             'some abstract',
+                                             'journal ref'),
+                         '%28cat:cs.AI+OR+cat:cs.INVENTED%29+AND+'
+                         'ti:%22some+title%22+AND+'
+                         'au:%22some+author%22+AND+'
+                         'abs:%22some+abstract%22+AND+'
+                         'jr:%22journal+ref%22')
+
+
+if __name__ == "__main__":
+    unittest.main()
