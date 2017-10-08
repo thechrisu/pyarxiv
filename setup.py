1376b86e19da164d1444c6d86b4157635004b9b0
culshoefer <christophsulshoefer@gmail.com>
2017-10-07 23:38:43 -0400

    Spiced up setup.ply

--- setup.py
+++ setup.py
@@ -2,13 +2,32 @@
 
 from distutils.core import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='pyarxiv',
       version='1.0',
       install_requires=requirements,
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Environment :: Console'
+      ],
       description='Python Client Library and CLI client for the ArXiv.org API',
+      long_description="See source on GitHub: https://github.com/culshoefer/pyarxiv\n" + readme,
       author='Christoph Ulshoefer',
       license='MIT',
       author_email='c@culshoefer.com',
@@ -16,4 +35,4 @@ setup(name='pyarxiv',
       url='https://github.com/culshoefer/pyarxiv/',
       scripts=['scripts/pyarxiv-cli'],
       test_suite='tests'
-)
+      )
